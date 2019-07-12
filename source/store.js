2d9d3993709c7705fdd45cec74bc9cfaf7e4a41c
SamOlivas <sam.a.olivas@gmail.com>
2019-07-12 16:01:27 -0700

    2nd commit

--- source/store.js
+++ source/store.js
@@ -1,5 +1,6 @@
 // show the most popular school
 // show the school with the highest GPA
+const { store } = require('redux')
 
 
 const ADD_STUDENT = 'ADD_STUDENT';
@@ -7,17 +8,50 @@ const DEL_STUDENT = 'DEL_STUDENT';
 const ENROLL_STUDENT = 'ENROLL_STUDENT';
 const WITHDRAW_STUDENT = 'WITHDRAW_STUDENT';
 
-
+const addStudent= (_student) => (
+  {
+    type: ADD_STUDENT,
+    student: _student
+  }
+);
+const delStudent = (_student) => (
+  {
+    type: DEL_STUDENT,
+    id: _student.id
+  }
+);
+// v TO WORK ON v
+const enrollStudent = (_student, _school) => (
+  {
+    type: ENROLL_STUDENT,
+    student: _student,
+    school: _school
+  }
+);
+const withdrawStudent = (_student) => (
+  {
+    type: WITHDRAW_STUDENT,
+    student: _student
+  }
+);
 
 const store = Redux.createStore((state = {schools: [], students: []}, action ) => {
   switch(action.type){
-    case ADD_STUDENT:
+    case 'ADD_STUDENT':
+      return {
+        ...state,
+        students: {...state.students, student}
+      }
       break;
-    case DEL_STUDENT:
+    case 'DEL_STUDENT':
+      return {
+        ...state,
+        students: state.students.filter( (student) => student.id !== action.id)
+      }
       break;
-    case ENROLL_STUDENT:
+    case 'ENROLL_STUDENT':
       break;
-    case WITHDRAW_STUDENT:
+    case 'WITHDRAW_STUDENT':
       break;
     default:
       return state
